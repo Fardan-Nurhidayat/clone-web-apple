cac4c89d3e89d79c4368192c102ab96f7e3df20c
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2025-01-03 21:28:09 +0700

    Add Script Js for carousel

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log('main.js');
+document.addEventListener("DOMContentLoaded", () => {
+  // Deklarasi index;
+  let currentIndex = 0;
+  const items = document.querySelectorAll(".carousel-item");
+  const controls = document.querySelectorAll(".carousel-control .items");
+  function setClasses(newIndex) {
+    items.forEach((item, i) => {
+      item.classList.remove(
+        "active",
+        "previous",
+        "next",
+        "move-left",
+        "move-right",
+        "enter-left",
+        "enter-right"
+      );
+      if (i === newIndex) {
+        item.classList.add("active");
+      } else if (i === (newIndex - 1 + items.length) % items.length) {
+        item.classList.add("previous");
+      } else if (i === (newIndex + 1) % items.length) {
+        item.classList.add("next");
+      }
+    });
+    controls.forEach((control, i) => {
+      control.classList.toggle("active", i === newIndex);
+    });
+  }
+  setClasses(0);
+});
